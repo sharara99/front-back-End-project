e3b693dc50ed40064822086e72d8e3bce43e1586
Hendawyy <seifhendawy1@gmailcom>
2025-03-02 10:28:01 +0200

    Trigger Push

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -1,17 +1,23 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
+
 const app = express();
 const port = 3000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*", // Change this to your frontend URL for security
+    methods: ["GET", "POST"],
+  })
+);
 
 let counter = 0;
 
-app.get('/api/increment', (req, res) => {
-    counter++;
-    res.json({ counter });
+app.get("/api/increment", (req, res) => {
+  counter++;
+  res.json({ counter });
 });
 
 app.listen(port, () => {
-    console.log(`Backend API listening at http://localhost:${port}`);
+  console.log(`Backend API listening at http://localhost:${port}`);
 });
--- FrontEnd/Dockerfile
+++ FrontEnd/Dockerfile
@@ -3,5 +3,3 @@ FROM nginx:latest
 COPY index.html /usr/share/nginx/html/index.html
 
 EXPOSE 80
-
-CMD ["nginx", "-g", "daemon off;"]
